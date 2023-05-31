9642a5fcc71d3356326e836059ccf7934198fa02
kkmanwork <jabawork015@gmail.com>
2023-05-31 20:29:16 +0800

    final version

--- products.py
+++ products.py
@@ -9,4 +9,7 @@ while True:
 	p.append(price)
 	# p = [name, price]可以簡略寫
 	products.append(p)#可以簡寫為products.append([name,price])
-print(products[0][1]#讀取0大清單中的1小清單位置
+print(products[0][1])#讀取0大清單中的1小清單位置
+for p in products:
+	print(p[0], "的價格是", p[1])
+	#print(p)
